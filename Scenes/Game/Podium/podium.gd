a308ddad80f66ca297f875cc4d8107777cf706af
Mark <markarneman@gmail.com>
2025-02-10 01:28:06 -0500

    podium changes

--- Scenes/Game/Podium/podium.gd
+++ Scenes/Game/Podium/podium.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	if !Global.game_started and body.is_in_group("player_hand"):
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if !Global.game_started and area.is_in_group("player_hand"):
+		Global.game_started = true
 		Events.start_game.emit()
