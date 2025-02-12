ced73f4f12431e0d009f0b529fca419cb032092c
Mark <markarneman@gmail.com>
2025-02-11 21:26:06 -0500

    Fixed crystal spawn

--- Scenes/Game/Crystals/pickable_crystal.gd
+++ Scenes/Game/Crystals/pickable_crystal.gd
@@ -38,12 +38,12 @@ func _on_level_3_completed() -> void:
 
 func crystal_hit() -> void:
 	hits += 1
-	mesh_instance_3d.scale -= Vector3(0.25, 0.25, 0.25)
+	mesh_instance_3d.scale -= Vector3(0.1, 0.1, 0.1)
 
 	if hits > 2:
 		var staff_head: RigidBody3D = STAFF_HEAD.instantiate()
 		staff_head.transform = transform
 		get_tree().get_first_node_in_group("base").add_child(staff_head)
-		get_tree().get_first_node_in_group("anvil_snap_zone").pickup_item(staff_head)
+		get_tree().get_first_node_in_group("anvil_snap_zone").pick_up_object(staff_head)
 		Events.spawn_staff_head.emit()
 		queue_free()
