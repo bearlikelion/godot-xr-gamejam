ccba5dee6b1efa4aa75872550a8355d162124418
Mark <markarneman@gmail.com>
2025-02-14 18:43:34 -0500

    Village houses

--- Scenes/Game/Chest/chest_key.gd
+++ Scenes/Game/Chest/chest_key.gd
@@ -3,3 +3,7 @@ class_name ChestKey
 extends XRToolsPickable
 
 @export var golden_key: bool = false
+
+
+func _on_dropped(_pickable: Variant) -> void:
+	freeze = false
--- Scenes/Player/Hat/hat_snap_zone.gd
+++ Scenes/Player/Hat/hat_snap_zone.gd
@@ -2,6 +2,7 @@
 class_name HatSnapZone
 extends XRToolsSnapZone
 
+var wore_hat: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +14,7 @@ func _on_chest_opened() -> void:
 
 
 func _on_has_picked_up(what: Variant) -> void:
-	if what is WizardHat:
-		what.reparent(get_tree().get_first_node_in_group("base"))
+	if what is WizardHat and not wore_hat:
+		wore_hat = true
+		what.reparent(get_tree().get_first_node_in_group("base"), true)
 		Events.player_equipped_hat.emit()
